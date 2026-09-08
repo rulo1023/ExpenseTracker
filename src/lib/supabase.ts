73663fb35a849fde7aafd7364d8efb2052796b4c
Rubén López de Juan <rubenlopezdejuan@gmail.com>
2026-09-08 19:52:47 +0200

    chore: configure Supabase client

---
+++ src/lib/supabase.ts
@@ -0,0 +1,23 @@
+import 'react-native-url-polyfill/auto';
+import 'expo-sqlite/localStorage/install';
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl =
+  process.env.EXPO_PUBLIC_SUPABASE_URL!;
+
+const supabaseKey =
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey,
+  {
+    auth: {
+      storage: localStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  }
+);
